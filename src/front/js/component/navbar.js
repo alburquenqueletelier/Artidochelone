7ad109ad90c9d9e161400b52577f92cb351eb80a
Lyss2120 <Lys.ivonne@gmail.com>
2022-07-31 19:17:36 +0000

    agregando slider

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -11,8 +11,8 @@ export const Navbar = () => {
   return (
 
     <>
-      <nav className="text-secondary navbar p-0 navbar-expand-md bg-light  ">
-        <div className="container-fluid glass1 p-4 ">
+      <nav className=" navbar navbar-dark p-0 navbar-expand-md bg-light  ">
+        <div className="container-fluid glassnav p-4 ">
 
 
           <a className="navbar-brand fs-4 text-secondary" href="/">ARTEDOCHELONE</a>
@@ -21,39 +21,39 @@ export const Navbar = () => {
           </button>
 
 
-          <div className="collapse navbar-collapse text-secondary " id="navbarSupportedContent">
+          <div className="collapse navbar-collapse  " id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto  mb-2 mb-lg-0 ">
               <li className="nav-item mx-3">
-                <a className="nav-link active text-secondary" aria-current="page" href="#"><BsChatLeftDots /></a>
+                <a className="nav-link active " aria-current="page" href="#"><BsChatLeftDots /></a> {/*onclick agregar clase =  active*/}
               </li>
               <li className="nav-item mx-3 ">
-                <a className="nav-link text-secondary" href="#"><MdOutlineAddPhotoAlternate /></a>
+                <a className="nav-link " href="#"><MdOutlineAddPhotoAlternate /></a>
               </li>
 
               <li className="nav-item mx-3">
-                <a className="nav-link text-secondary"><BsHeart className="icons"/></a>
+                <a className="nav-link "><BsHeart className="icons"/></a>
               </li>
 
               <li className="nav-item mx-3">
                 <form className="d-flex m-auto p-0" role="search">
-                  <button className="btn text-secondary " type="submit"><BsSearch /></button> {/*al clickearlo se abre el input click input .visible sino .invisible*/}
+                  <button className="btn  " type="submit"><BsSearch /></button> {/*añadir tooltips a los iconos al clickearlo se abre el input click input .visible sino .invisible*/}
                   <input className="form-control me-2 invisible" type="search" placeholder="Search" aria-label="Search" />
                 </form>
               </li>
             </ul>
 
-
-
-
-
             <div className="nav-item dropdown ">
               <a className="nav-link dropdown-toggle text-secondary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Perfil              </a>
+                Perfil              
+              </a>
               <ul className="dropdown-menu dropdown-menu-end">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Mi perfil</a></li>
+                <li><a className="dropdown-item" href="#">Seguidos </a></li>
+                <li><a className="dropdown-item" href="#">Guardados</a></li>
+                <li><a className="dropdown-item" href="#">Mensajes</a></li>
+
                 <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#">Log Out</a></li>
               </ul>
             </div>
 
