698eab76f82f9d5c34ffccecbc98665bb00a72ea
Demi4nT <demhernandezmont21@gmail.com>
2022-08-04 21:19:04 +0000

    cambios register

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User, Post, Comment, Hashtag
 from api.utils import generate_sitemap, APIException
+from flask_cors import CORS
 #import datetime
 #from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
