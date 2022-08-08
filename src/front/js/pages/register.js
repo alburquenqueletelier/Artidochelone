362e6b31dc777535cabfddf0d0a6f4ae3315fee5
alburquenqueletelier <alburquenque.letelier@gmail.com>
2022-08-08 18:58:13 +0000

    agrego funcionalidad google login. empenzando pruebas

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -18,44 +18,57 @@ export const Register = () => {
 
   const handleFailure = (result) => {
     alert(result);
-  }
-  
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name.length < 1 || typeof name.charAt(0) === number || name.charAt(0) == ' ') {
-      alert('Name Error: must be greater than 1, start with a letter (don\'t use space or especial character');
+    if (
+      name.length < 1 ||
+      typeof name.charAt(0) === number ||
+      name.charAt(0) == " "
+    ) {
+      alert(
+        "Name Error: must be greater than 1, start with a letter (don't use space or especial character"
+      );
       return false;
     }
-    if (lastname.length < 1 || typeof lastname.charAt(0) === number || lastname.charAt(0) == ' ') {
-      alert('Lastname Error: must be greater than 1, start with a letter (don\'t use space or especial character');
+    if (
+      lastname.length < 1 ||
+      typeof lastname.charAt(0) === number ||
+      lastname.charAt(0) == " "
+    ) {
+      alert(
+        "Lastname Error: must be greater than 1, start with a letter (don't use space or especial character"
+      );
       return false;
     }
     if (password != password2) {
-      alert('Password must coincide');
+      alert("Password must coincide");
       return false;
     }
-    if (!email.includes('@')) {
-      alert('Enter a valid email');
+    if (!email.includes("@")) {
+      alert("Enter a valid email");
       return false;
     }
     setInfo(data), actions.postRegister(data);
   };
   let data = {
-    name: name.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase()))),
-    lastname: lastname.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase()))),
+    name: name.replace(/\w\S*/g, (w) =>
+      w.replace(/^\w/, (c) => c.toUpperCase())
+    ),
+    lastname: lastname.replace(/\w\S*/g, (w) =>
+      w.replace(/^\w/, (c) => c.toUpperCase())
+    ),
     username: username,
     email: email,
 
     password: password,
-    password2: password2
+    password2: password2,
   };
   return (
-
     <div className="my-4 mx-auto p-4 rounded col-4 glass2">
       <div className="border border-light border-bottom-0 text-light rounded-top">
-        <h3 className="m-3">
-          Sign Up
-        </h3>
+        <h3 className="m-3">Sign Up</h3>
       </div>
       <form>
         <div className="mb-3">
@@ -110,13 +123,12 @@ export const Register = () => {
             className="form-control bg-default opacity-50"
             placeholder="Repeat Password"
             id="exampleInputPassword2"
-            onChange={(e)=> setPassword2(e.target.value)}
+            onChange={(e) => setPassword2(e.target.value)}
           />
         </div>
         <div className="d-grid gap-2 my-3 ">
           <button
             type="submit"
-
             className="btn btn-outline-dark opacity-70 my-3 "
             onClick={(e) => {
               handleSubmit(e);
@@ -129,23 +141,13 @@ export const Register = () => {
           {/* <button className="btn btn-outline-light" onClick={()=>actions.googleLogin()}> */}
           <GoogleLogin
             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-            buttonText={"Or continue with"+<FcGoogle className="fs-2"/>}
+            buttonText={"Or continue with" + <FcGoogle className="fs-2" />}
             onSuccess={actions.googleLogin}
             onFailure={handleFailure}
             cookiePolicy={"single_host_origin"}
-          >
-
-          </GoogleLogin>
+          ></GoogleLogin>
         </div>
       </form>
-
     </div>
-    <div className="d-grid gap-2 my-3">
-      <button className="btn btn-outline-light">
-        Or continue with <FcGoogle className="fs-2" />
-      </button>
-    </div>
-      </form >
-    </div >
   );
 };
