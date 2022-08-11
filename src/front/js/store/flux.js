8b38cc681cd34615ed4ea057ded151b8c5fa8ef1
alburquenqueletelier <alburquenque.letelier@gmail.com>
2022-08-11 16:26:25 +0000

    probando solo con front end

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -139,53 +139,6 @@ const getState = ({ getStore, getActions, setStore }) => {
       profiles: [],
     },
     actions: {
-      postRegister: async (data) => {
-        console.log(data);
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        var raw = JSON.stringify(data);
-
-        var requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: raw,
-          redirect: "follow",
-        };
-
-        await fetch(process.env.BACKEND_URL + "/api/register", requestOptions)
-          .then((response) => response.json())
-          .then((result) => {
-            console.log(result);
-            alert("Successfully registered");
-          })
-          .catch((error) => console.log("error !!!", data, error));
-      },
-      // login fetch
-      login: async (event, user, pass) => {
-        event.preventDefault();
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        let requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          cors: "no-cors",
-          body: JSON.stringify({
-            username: user,
-            password: pass,
-          }),
-        };
-        await fetch(process.env.BACKEND_URL + "/api/login", requestOptions)
-          .then((response) => response.json())
-          .then((data) => {
-            // deberia guardar toda la info de user que esta en el serializador
-            console.log(data);
-            sessionStorage.setItem("user", JSON.stringify(data.user));
-            return setStore({ user: data.user });
-          })
-          .catch((error) => console.log("mensaje error: ", error));
-      },
       loginRemember: () =>
         setStore({ user: JSON.parse(sessionStorage.getItem("user")) }),
       logout: () => {
@@ -220,68 +173,19 @@ const getState = ({ getStore, getActions, setStore }) => {
           //   console.log(imageData);
           // })
           .catch((error) => console.log(error));
-
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        const { imageData } = getStore();
-
-        let requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: JSON.stringify({
-            image: imageData.url,
-            title: title,
-            description: description,
-          }),
-        };
-        // segunda api
-        // console.log("url: ", imageData.url, "type", typeof imageData.url);
-        // console.log("requestopti: ", requestOptions);
-        await fetch(process.env.BACKEND_URL + "/api/post", requestOptions)
-          .then((response) => {
-            console.log("pase por response");
-            return response.json();
-          })
-          .then((data) => {
-            return console.log(data);
-          })
-          .catch((error) => console.log("mensaje error: ", error));
-        // console.log("fileUp: ", e.target.fileUp.value);
-        // console.log("title: ", e.target.title.value);
-        // console.log("description: ", e.target.description.value);
-        // console.log("file", file)
       },
-      // demoLogin:() => {
-      //   fetch(process.env.BACKEND_URL + "/api/hello", requestOptions)
-      //   const {login, user} = getStore();
-      //   if (!login){
-      //     setStore({login: true})
-      //     setStore({user: "baal"})
-      //   } else {
-      //     setStore({login: false})
-      //     setStore({user: null})
-      //   }
-      // },
-
-      //getProfiles: async () => {
-
-      //var requestOptions = {
-      //   method: 'GET',
-      //  redirect: 'follow'
-      // };
-
-      //await fetch(process.env.BACKEND_URL + "/api/alluser", requestOptions)
-      //  .then(response => response.json())
-      //  .then(result => { console.log(result),
-      //    setStore({profiles: result})})
-      //  .catch(error => console.log('error', error));
-
-      // }
+      demoLogin:() => {
+        fetch(process.env.BACKEND_URL + "/api/hello", requestOptions)
+        const {login, user} = getStore();
+        if (!login){
+          setStore({login: true})
+          setStore({user: "baal"})
+        } else {
+          setStore({login: false})
+          setStore({user: null})
+        }
+      },
     },
-    demoActions: {
-
-    }
   };
 };
 
